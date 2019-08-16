f470802ea5235d7f7624a466829ab7af09a59ed2
Karen Payne <paynekaren@comcast.net>
2019-08-16 12:25:50 -0700

    Moved from LINQ to Lambda for EF Starts with code sample

--- EntityFrameworkCoreLikeLibrary/EntityFrameworkCoreOperations.cs
+++ EntityFrameworkCoreLikeLibrary/EntityFrameworkCoreOperations.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using EntityFrameworkCoreLikeLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.EF;
 
 namespace EntityFrameworkCoreLikeLibrary
 {
@@ -10,27 +12,50 @@ namespace EntityFrameworkCoreLikeLibrary
         {
             using (var context = new NorthWindContext())
             {
-                var customerData = (
-                    from customer in context.Customers
-                    join contactType in context.ContactType on customer.ContactTypeIdentifier equals contactType.ContactTypeIdentifier
-                    join contact in context.Contact on customer.ContactIdentifier equals contact.ContactIdentifier
-                    where customer.CompanyName.StartsWith(pContains) && customer.ContactTypeIdentifier == 5
-                    select new CustomerEntity
+                var customerData = (context.Customers
+                    .Join(context.ContactType, customer => customer.ContactTypeIdentifier,
+                        contactType => contactType.ContactTypeIdentifier,
+                        (customer, contactType) => new {customer, contactType}) 
+                    .Join(context.Contact, cust => cust.customer.ContactIdentifier, contact => contact.ContactIdentifier,
+                        (custContType, contact) => new {item = custContType, contact})
+                    .Where(items =>
+                        items.@item.customer.ContactTypeIdentifier == 5 && 
+                        /*
+                         * Here is the starts with clause
+                         */
+                        Functions.Like(items.@item.customer.CompanyName, pContains))
+                    .Select(item => new CustomerEntity
                     {
-                        CustomerIdentifier = customer.CustomerIdentifier,
-                        CompanyName = customer.CompanyName,
-                        ContactIdentifier = customer.ContactIdentifier,
-                        FirstName = contact.FirstName,
-                        LastName = contact.LastName,
-                        ContactTypeIdentifier = contactType.ContactTypeIdentifier,
-                        ContactTitle = contactType.ContactTitle,
-                        City = customer.City,
-                        PostalCode = customer.PostalCode,
-                        CountryIdentifier = customer.CountryIdentfier, 
-                        CountyName = customer.CountryIdentfierNavigation.CountryName
-                    }).ToList();
+                        CustomerIdentifier = item.@item.customer.CustomerIdentifier,
+                        CompanyName = item.@item.customer.CompanyName,
+                        ContactIdentifier = item.@item.customer.ContactIdentifier,
+                        FirstName = item.contact.FirstName,
+                        LastName = item.contact.LastName,
+                        ContactTypeIdentifier = item.@item.contactType.ContactTypeIdentifier,
+                        ContactTitle = item.@item.contactType.ContactTitle,
+                        City = item.@item.customer.City,
+                        PostalCode = item.@item.customer.PostalCode,
+                        CountryIdentifier = item.@item.customer.CountryIdentfier,
+                        CountyName = item.@item.customer.CountryIdentfierNavigation.CountryName
+                    })).ToList();
 
                 return customerData;
+
+            }
+        }
+        public List<CustomerEntity> GetCustomersWithContainsFunc(string pContains) 
+        {
+            using (var context = new NorthWindContext())
+            {
+                var customerData = (context.Customers
+                    .Join(context.ContactType, customer => customer.ContactTypeIdentifier,
+                        contactType => contactType.ContactTypeIdentifier,
+                        (customer, contactType) => new {customer, contactType})
+                    .Join(context.Contact, @t => @t.customer.ContactIdentifier, contact => contact.ContactIdentifier,
+                        (@t, contact) => @t.customer)).Where(c => Functions.Like(c.CompanyName, "%horn%"));
+
+                
+                return new List<CustomerEntity>();
             }
         }
     }
